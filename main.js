c06b553bf1604904325815d425607db7d9083ef8
jon sautel <jon.sautel07@gmail.com>
2022-04-15 10:58:13 +0200

    ajout fichier scss + photo + bouton on-off video

---
+++ main.js
@@ -0,0 +1,16 @@
+const buttonOnAudio = document.getElementById('onAudio');
+const buttonOffAudio = document.getElementById('offAudio');
+const playerVideo = document.getElementById('videoPlayer');
+
+function onAudio(){
+playerVideo.muted = false
+buttonOffAudio.classList.add('hiddenButton');
+buttonOnAudio.classList.remove('hiddenButton');
+}
+
+function offAudio(){
+    playerVideo.muted = true;
+    buttonOnAudio.classList.add('hiddenButton');
+    buttonOffAudio.classList.remove('hiddenButton');
+
+}
