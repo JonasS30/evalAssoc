701bcd3083bb9affd074f57a845d160a2ac93b92
jon sautel <jon.sautel07@gmail.com>
2022-04-26 12:02:03 +0200

    ajout contenu page legendaire

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ include '_include/_head.php'
             <div class="videoContainer">
 
                 <video autoplay muted loop id="videoPlayer">
-                    <source src="https://jonnas.ovh/index.php/s/DEAJ4pdJM5XP79q/download/LEGENDAIRE%202018%20SAINT%20LAURENT%20DES%20ARBRES%20-%20EVENT_x264.mp4" type="video/mp4">
+                    <source src="https://jonnas.ovh/index.php/s/9qxxXPPQnNwtWDy/download/LEGENDAIRE%202018%20SAINT%20LAURENT%20DES%20ARBRES%20-%20EVENT_x264.mp4" type="video/mp4">
 
                     Désolé, votre navigateur ne supporte pas la vidéo intégrées.
                 </video>
@@ -27,23 +27,90 @@ include '_include/_head.php'
                     <img src="icon/volume1.png" alt="icon audio on">
                 </button>
             </div>
+            <img src="./icon/Composant 4 – 1.png" alt="déco" class="banderole">
 
-            <div class="content1">
-                <div class="part1">
-                    <img src="./photos/légendaire8.png" alt="logo Association">
-                </div>
-
-                <div class="part2">
-
-                    <h1>
-                        Le légendaire de Saint Laurent
-                    </h1>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam eveniet odit dolores eos. Ipsum velit facilis, earum soluta deleniti pariatur, dolores magnam vel dolore totam culpa quos, exercitationem ut ipsa.<br>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime molestias at, officiis doloribus veniam laborum dolores eum architecto sapiente ipsam libero nesciunt magnam ex iure tenetur voluptas optio corporis minima.<br><br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem dolores porro earum dolore inventore libero repellendus iure doloribus veritatis cum? Provident recusandae laboriosam, veniam a nemo est quos aliquid rem.
-                    </p>
-                </div>
-            </div>
+            
         </section>
+
+        <div class="container-fluid">
+	<div class="row part1 align-items-center">
+		<div class="col-md-4 ">
+			<img alt="Logo association Légendaire Saint laurent des Arbres" src="./photos/légendaire8.png">
+		</div>
+		<div class="col-md-8 content1 ">
+			<h1>
+				Légendaire de Saint Laurent
+			</h1>
+			<p>
+				Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
+                <br>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde iure cupiditate ut id? Distinctio assumenda architecto sunt! Assumenda, molestias eveniet, soluta voluptatibus delectus nihil eligendi nisi eaque quam dolores repudiandae!
+			</p>
+			
+		</div>
+	</div>
+	<div class="row part2 align-items-center">
+		<div class="col-md-8 content2">
+        <h2>
+                    l'histoire
+                </h2>
+			<p>
+				Lorem ipsum dolor sit amet, <strong>consectetur adipiscing elit</strong>. Aliquam eget sapien sapien. Curabitur in metus urna. In hac habitasse platea dictumst. Phasellus eu sem sapien, sed vestibulum velit. Nam purus nibh, lacinia non faucibus et, pharetra in dolor. Sed iaculis posuere diam ut cursus. <em>Morbi commodo sodales nisi id sodales. Proin consectetur, nisi id commodo imperdiet, metus nunc consequat lectus, id bibendum diam velit et dui.</em> Proin massa magna, vulputate nec bibendum nec, posuere nec lacus. <small>Aliquam mi erat, aliquam vel luctus eu, pharetra quis elit. Nulla euismod ultrices massa, et feugiat ipsum consequat eu.</small>
+                <br>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde iure cupiditate ut id? Distinctio assumenda architecto sunt! Assumenda, molestias eveniet, soluta voluptatibus delectus nihil eligendi nisi eaque quam dolores repudiandae!
+			</p>
+		</div>
+		<div class="col-md-4">
+            <div class="deco">
+                <img alt="image" src="./photos/3.jpg">
+
+            </div>
+		</div>
+	</div>
+	<div class="row part3">
+                <h2>
+                L'association 
+                </h2>
+         
+		<div class="col-md-4">
+			<p>
+				Lorem ipsum dolor sit amet, <strong>consectetur adipiscing elit</strong>. Aliquam eget sapien sapien. Curabitur in metus urna. In hac habitasse platea dictumst. Phasellus eu sem sapien, sed vestibulum velit. Nam purus nibh, lacinia non faucibus et, pharetra in dolor. Sed iaculis posuere diam ut cursus. <em>Morbi commodo sodales nisi id sodales. Proin consectetur, nisi id commodo imperdiet, metus nunc consequat lectus, id bibendum diam velit et dui.</em> Proin massa magna, vulputate nec bibendum nec, posuere nec lacus. <small>Aliquam mi erat, aliquam vel luctus eu, pharetra quis elit. Nulla euismod ultrices massa, et feugiat ipsum consequat eu.</small>
+			</p>
+		</div>
+		<div class="col-md-4">
+			<p>
+				Lorem ipsum dolor sit amet, <strong>consectetur adipiscing elit</strong>. Aliquam eget sapien sapien. Curabitur in metus urna. In hac habitasse platea dictumst. Phasellus eu sem sapien, sed vestibulum velit. Nam purus nibh, lacinia non faucibus et, pharetra in dolor. Sed iaculis posuere diam ut cursus. <em>Morbi commodo sodales nisi id sodales. Proin consectetur, nisi id commodo imperdiet, metus nunc consequat lectus, id bibendum diam velit et dui.</em> Proin massa magna, vulputate nec bibendum nec, posuere nec lacus. <small>Aliquam mi erat, aliquam vel luctus eu, pharetra quis elit. Nulla euismod ultrices massa, et feugiat ipsum consequat eu.</small>
+			</p>
+		</div>
+		<div class="col-md-4">
+			<p>
+				Lorem ipsum dolor sit amet, <strong>consectetur adipiscing elit</strong>. Aliquam eget sapien sapien. Curabitur in metus urna. In hac habitasse platea dictumst. Phasellus eu sem sapien, sed vestibulum velit. Nam purus nibh, lacinia non faucibus et, pharetra in dolor. Sed iaculis posuere diam ut cursus. <em>Morbi commodo sodales nisi id sodales. Proin consectetur, nisi id commodo imperdiet, metus nunc consequat lectus, id bibendum diam velit et dui.</em> Proin massa magna, vulputate nec bibendum nec, posuere nec lacus. <small>Aliquam mi erat, aliquam vel luctus eu, pharetra quis elit. Nulla euismod ultrices massa, et feugiat ipsum consequat eu.</small>
+			</p>
+		</div>
+	</div>
+	<div class="row part4">
+		<div class="col-md-2">
+		</div>
+		<div class="col-md-8">
+            <div class="deco">
+
+                <img alt="Photo du village de Saint Laurent des Arbres" src="./photos/saint-laurent-des-arbres-mairie.jpg">
+            </div>
+		</div>
+		<div class="col-md-2">
+		</div>
+	</div>
+	<div class="row part5">
+		<h2>
+			La convivialité
+		</h2>
+		<div class="col-md-12">
+			<p>
+				Lorem ipsum dolor sit amet, <strong>consectetur adipiscing elit</strong>. Aliquam eget sapien sapien. Curabitur in metus urna. In hac habitasse platea dictumst. Phasellus eu sem sapien, sed vestibulum velit. Nam purus nibh, lacinia non faucibus et, pharetra in dolor. Sed iaculis posuere diam ut cursus. <em>Morbi commodo sodales nisi id sodales. Proin consectetur, nisi id commodo imperdiet, metus nunc consequat lectus, id bibendum diam velit et dui.</em> Proin massa magna, vulputate nec bibendum nec, posuere nec lacus. <small>Aliquam mi erat, aliquam vel luctus eu, pharetra quis elit. Nulla euismod ultrices massa, et feugiat ipsum consequat eu.</small>
+			</p>
+		</div>
+	</div>
+</div>
 
 
 
