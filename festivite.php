532422d20d01a1c6cefd123e506face689782898
jon sautel <jon.sautel07@gmail.com>
2022-04-28 16:35:39 +0200

    ajout page festivité

--- festivite.php
+++ festivite.php
@@ -1,10 +1,60 @@
 <?php
 include '_include/_head.php'
 ?>
-<body>
+<body id="page_festivite">
 <?php
 include '_include/_menu.php'
 ?>
+<div class="container">
+    <div class="row">
+        <div class="col col-md-8 " >
+            <article>
+                <h1>
+                    Légendaire de Saint Laurent 2022
+                </h1>
+                <p>
+                    Venez nombreux pour fêter la Légendaire de Saint Laurent.a patir de 14h jusqu'à dans la soirée. <br> On vous attends avec impatience le samedi 27 Août 2022.
+                    Chasse au trésor, animations gratuites et concert sont troujours au rendez vous !
+                </p>
+
+                <div class="deco">
+                    <img src="./photos/affiche.jpg" alt="Affiche de la manifestation">
+
+                </div>
+               
+
+                <div class="deco">
+                    <a href="">
+
+                        <img src="./photos/planning.jpg" alt="planning manifestation">
+                    </a>
+                </div>
+            </article>
+    
+        </div>
+        <aside class="col col-md-4">
+            <ul class="list-group list-group-flush mt-4">
+                <li class="list-group-item list-group-item-action active">
+                    <a href="#">Légendaire de Saint Laurent 2022</a>
+                </li>
+                <li class="list-group-item list-group-item-action">
+                    <a href="#">Les ateliers "PATRIMOINE-HISTOIRE"</a>
+                </li>
+                <li class="list-group-item list-group-item-action">
+                    <a href="#">Légendaire de Saint Laurent 2021</a>
+                </li>
+                <li class="list-group-item list-group-item-action">
+                    <a href="#">Réunion publique centre Pierre Garcia</a>
+                </li>
+
+            </ul>
+           
+        </aside>
+            
+        </div>
+    </div>
+</div>
+
 
 
     <?php
